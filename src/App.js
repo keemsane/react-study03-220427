00ea27c944665cdbd74253f59d975644e200b559
keemsane <keemsane@gmail.com>
2022-04-27 12:04:33 +0900

    리액트 부트스트랩 준비

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 리액트용 부트스트랩 사용하기
+// 리액트용 부트스트랩 설치
+// > yarn add react-bootstrap bootstrap
+
+// 리액트용 부트스트랩 로드 방법 2 가지
+// 1. import Button from 'react-bootstrap/Button';
+// 2. import { Button } from 'react-bootstrap';
+
+// <Button>부트스트랩 버튼</Button>
 function App() {
   return (
     <div className="App">
